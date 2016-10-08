5c85b68468e52c92463b53f6036b1560d7c2d4ac
shuheiktgw <shuhei.kitagawa@gmail.com>
2016-10-08 16:54:01 +0900

    first commti

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Shuhei on 2016/10/08.
+ */
+export default function(){
+    return[
+        {title: "Javascript: The Good Parts"},
+        {title: "Harry Potter"},
+        {title: "The Dark Tower"},
+        {title: "Eloquent Ruby"}
+    ]
+}
